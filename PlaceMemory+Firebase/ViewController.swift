b6b8f8e4a0a809e9beba6b49a62e353b85f5532b
aney <yenafirst91@gmail.com>
2017-05-01 03:14:39 +0900

    First commit

---
+++ PlaceMemory+Firebase/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PlaceMemory+Firebase
+//
+//  Created by aney on 2017. 5. 1..
+//  Copyright © 2017년 Taedong Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
+
