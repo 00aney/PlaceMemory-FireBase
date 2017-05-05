4cf746726278a4935ab3e56d37a449de0d9c9357
aney <yenafirst91@gmail.com>
2017-05-06 03:16:49 +0900

    TabBarController 아이템 추가

--- PlaceMemory+Firebase/Sources/ViewControllers/MainTabBarController.swift
+++ PlaceMemory+Firebase/Sources/ViewControllers/MainTabBarController.swift
@@ -16,14 +16,38 @@ class MainTabBarController: UITabBarController {
   // MARK: View Life Cycle
   
   override func viewDidLoad() {
-    let userProfileViewController = UserProfileViewController(collectionViewLayout: UICollectionViewFlowLayout())
-    let navigationController = UINavigationController(rootViewController: userProfileViewController)
+    let homeNavagationController = self.templateNavigationController(unselectedImage: #imageLiteral(resourceName: "tab-home-unselected"), selectedImage: #imageLiteral(resourceName: "tab-home-selected"), rootViewController: UserProfileViewController(collectionViewLayout: UICollectionViewFlowLayout()))
     
-    navigationController.tabBarItem.image = #imageLiteral(resourceName: "tabfeed-profile-unselected")
-    navigationController.tabBarItem.selectedImage = #imageLiteral(resourceName: "tabfeed-profile-selected")
+    let searchNavagationController = self.templateNavigationController(unselectedImage: #imageLiteral(resourceName: "tab-search-unselected"), selectedImage: #imageLiteral(resourceName: "tab-search-selected"), rootViewController: UIViewController())
+    
+    let plusNavagationController = self.templateNavigationController(unselectedImage: #imageLiteral(resourceName: "tab-upload"), selectedImage: #imageLiteral(resourceName: "tab-upload"), rootViewController: UIViewController())
+    
+    let likeNavagationController = self.templateNavigationController(unselectedImage: #imageLiteral(resourceName: "icon-like-unselected"), selectedImage: #imageLiteral(resourceName: "icon-like-selected"), rootViewController: UIViewController())
+    
+    let userProfileNavagationController = self.templateNavigationController(unselectedImage: #imageLiteral(resourceName: "tab-profile-unselected"), selectedImage: #imageLiteral(resourceName: "tab-profile-selected"), rootViewController: UserProfileViewController(collectionViewLayout: UICollectionViewFlowLayout()))
+    
     self.tabBar.tintColor = .black
     
-    self.viewControllers = [navigationController, UIViewController()]
+    self.viewControllers = [
+      homeNavagationController,
+      searchNavagationController,
+      plusNavagationController,
+      likeNavagationController,
+      userProfileNavagationController,
+    ]
+    
+    guard let items = self.tabBar.items else { return }
+    for item in items {
+      item.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
+    }
+  }
+  
+  fileprivate func templateNavigationController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController = UIViewController()) -> UINavigationController {
+    let viewController = rootViewController
+    let navigationController = UINavigationController(rootViewController: viewController)
+    navigationController.tabBarItem.image = unselectedImage
+    navigationController.tabBarItem.selectedImage = selectedImage
+    return navigationController
   }
   
 }
